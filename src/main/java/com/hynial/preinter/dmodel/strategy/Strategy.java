c17e80d1f41d3a64d424b6de1a0bce8ca2e665d1
hynial.chen <hynials@gmail.com>
2021-01-17 15:45:11 +0800

    dmodel - strategy

---
+++ src/main/java/com/hynial/preinter/dmodel/strategy/Strategy.java
@@ -0,0 +1,5 @@
+package com.hynial.preinter.dmodel.strategy;
+
+public interface Strategy {
+    public int doOperation(int num1, int num2);
+}
---
+++ src/main/java/com/hynial/preinter/dmodel/strategy/StrategyApplication.java
@@ -0,0 +1,17 @@
+package com.hynial.preinter.dmodel.strategy;
+
+/**
+ * 策略模式
+ */
+public class StrategyApplication {
+    public static void main(String[] args) {
+        Context context = new Context(new OperationAdd());
+        System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
+
+        context = new Context(new OperationSubtract());
+        System.out.println("10 - 5 = " + context.executeStrategy(10, 5));
+
+        context = new Context(new OperationMultiply());
+        System.out.println("10 * 5 = " + context.executeStrategy(10, 5));
+    }
+}
